8a333440a817e3f446b3406b8109811ba92c47af
Unknown <1528357474@qq.com>
2018-12-07 09:00:49 +0800

    update

---
+++ SSM/ssm/src/main/java/com/demos/Application.java
@@ -0,0 +1,27 @@
+package com.demos;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+@EnableTransactionManagement
+@SpringBootApplication
+@MapperScan("com.demos.mapper")
+@ServletComponentScan
+public class Application extends SpringBootServletInitializer{
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(Application.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
